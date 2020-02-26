2cd29c68186f3ceb785015d788199cf8c3d359fc
Oleg <novganson@gmail.com>
2020-02-26 17:51:20 +0200

    added my github name

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <header className={'header'}>
+            <img src="https://pngimage.net/wp-content/uploads/2018/06/random-logo-png-9.png" alt="img"/>
+        </header>
+    );
+}
+
+export default Header;
