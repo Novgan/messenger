87623f06ff28619497083ec229d8c08c92f93d46
Oleg <novganson@gmail.com>
2020-03-01 03:15:41 +0200

    Add Diagram

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,23 +1,7 @@
 import React from "react";
 import classes from './Dialogs.module.css';
-import {NavLink} from "react-router-dom";
-
-const Dialog_items = (props) => {
-    let path = "/dialogs/" + props.id;
-    return (
-        <div className={classes.dialog}>
-            <NavLink to={path}>
-                {props.name}
-            </NavLink>
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return (
-        <div className={classes.message}>{props.content}</div>
-    )
-}
+import Dialog_items from "./DialogItem/DialogItem";
+import Message from "./Messages/Message";
 
 
 const Dialogs = (props) => {
